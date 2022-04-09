1c3565b3df60c04e427ec49f65e663e9e2388831
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-10 02:28:54 +0600

    Alhamdulillah.Sign in and sign out is completed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
-            <h3>Please Login.</h3>
+            <h3>Please Login Now</h3>
+            <div style={{ margin: "20px" }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form action="">
+                <input type="email" name="" id="" placeholder='Your Email' /> <br />
+                <input type="password" name="" id="" placeholder='Your Password' /> <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
